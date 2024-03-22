60400c65ba9ac858d898f90fc48514c4704cff06
rayhandestian <rayhandes7@gmail.com>
2024-03-22 15:32:00 +0700

    Remove unecessary comment

--- Pertemuan_4/Latihan_3/src/Armor.java
+++ Pertemuan_4/Latihan_3/src/Armor.java
@@ -2,13 +2,11 @@ public class Armor {
     public double defensePower;
     public String name;
 
-    // Constructor
     public Armor(String name, double defensePower) {
         this.name = name;
         this.defensePower = defensePower;
     }
 
-    // Display method
     public void display() {
         System.out.println("Armor: " + name + ", Defense Power: " + defensePower);
     }
--- Pertemuan_4/Latihan_3/src/Weapon.java
+++ Pertemuan_4/Latihan_3/src/Weapon.java
@@ -2,13 +2,11 @@ public class Weapon {
     public double attackPower;
     public String name;
 
-    // Constructor
     public Weapon(String name, double attackPower) {
         this.name = name;
         this.attackPower = attackPower;
     }
 
-    // Display method
     public void display() {
         System.out.println("Weapon: " + name + ", Attack Power: " + attackPower);
     }
